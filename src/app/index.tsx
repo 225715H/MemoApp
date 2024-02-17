0ba6742fc1f00853fd8ff232935c502d95cc3703
motoki higa <e225715@ie.u-ryukyu.ac.jp>
2024-02-17 14:22:39 +0900

    create memolist

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,40 +2,41 @@ import { Text, View, StyleSheet } from 'react-native'
 
 const Index = (): JSX.Element => {
   return (
-    <View style={ styles.container}>
-      <View>
-        <View>
-          <Text>Memo App</Text>
-          <Text>Logout</Text>
+    <View style={styles.container}>
+
+      <View style={styles.header}>
+        <View style={styles.headerInner}>
+          <Text style={styles.headerTitle}>Memo App</Text>
+          <Text style={styles.headerRight}>Logout</Text>
         </View>
       </View>
 
       <View>
 
-        <View>
+        <View style={styles.memoListItem}>
           <View>
-            <Text>買い物リスト</Text>
-            <Text>2024/02/17</Text>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2024/02/17</Text>
           </View>
           <View>
             <Text>X</Text>
           </View>
         </View>
 
-        <View>
+        <View style={styles.memoListItem}>
           <View>
-            <Text>買い物リスト</Text>
-            <Text>2024/02/17</Text>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2024/02/17</Text>
           </View>
           <View>
             <Text>X</Text>
           </View>
         </View>
 
-        <View>
+        <View style={styles.memoListItem}>
           <View>
-            <Text>買い物リスト</Text>
-            <Text>2024/02/17</Text>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2024/02/17</Text>
           </View>
           <View>
             <Text>X</Text>
@@ -44,8 +45,8 @@ const Index = (): JSX.Element => {
 
       </View>
 
-      <View>
-        <Text>+</Text>
+      <View style={styles.circleButton}>
+        <Text style={styles.circleButtonLabel}>+</Text>
       </View>
     </View>
   )
@@ -54,8 +55,71 @@ const Index = (): JSX.Element => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#ffffff'
+  },
+  header: {
+    backgroundColor: '#46b',
+    height: 104,
+    justifyContent: 'flex-end'
+  },
+  headerInner: {
     alignItems: 'center'
+  },
+  headerTitle: {
+    color: '#ffffff',
+    fontSize: 22,
+    lineHeight: 32,
+    fontWeight: 'bold',
+    marginBottom: 8
+  },
+  headerRight: {
+    color: 'rgba(255, 255, 255, 0.7)',
+    position: 'absolute',
+    right: 16,
+    bottom: 16
+  },
+  memoListItem: {
+    backgroundColor: '#ffffff',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 16,
+    paddingHorizontal: 19,
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: 'rgba(0, 0, 0, 0.15)'
+  },
+  memoListItemTitle: {
+    fontSize: 16,
+    lineHeight: 32
+  },
+  memoListItemDate: {
+    fontSize: 12,
+    lineHeight: 16,
+    color: '#848484'
+  },
+  circleButton: {
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    backgroundColor: '#46b',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    right: 40,
+    bottom: 40,
+    shadowColor: '#000',
+    // iOS用Shadow
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 8 },
+    shadowRadius: 8,
+    // Android用Shadow
+    elevation: 8
+  },
+  circleButtonLabel: {
+    color: '#ffffff',
+    fontSize: 40,
+    lineHeight: 40,
+    textAlign: 'center'
   }
 })
 
