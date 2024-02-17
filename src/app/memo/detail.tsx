541474a1c1bac83697cda6d5c38478ec7d4e78be
motoki higa <e225715@ie.u-ryukyu.ac.jp>
2024-02-17 15:54:11 +0900

    create edit frame

---
+++ src/app/memo/detail.tsx
@@ -0,0 +1,60 @@
+import { View, Text, StyleSheet, ScrollView } from 'react-native'
+// import { Feather } from '@expo/vector-icons'
+
+import Header from '../../components/header'
+import CircleButton from '../../components/CircleButton'
+import Icon from '../../components/Icon'
+
+const Detail = (): JSX.Element => {
+  return (
+    <View style={styles.container}>
+      <Header />
+      <View style={styles.memoHeader}>
+        <Text style={styles.memoTitle}>買い物リスト</Text>
+        <Text style={styles.memoDate}>2024/02/17</Text>
+      </View>
+      <ScrollView style={styles.memoBody}>
+        <Text style={styles.memoBodyText}>Detail</Text>
+      </ScrollView>
+      <CircleButton style={{ top: 160, bottom: 'auto' }}>
+        {/* <Feather name="edit-2" size={24} color="#ffffff" /> */}
+        <Icon name='pencil' size={40} color='#ffffff'/>
+      </CircleButton>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff'
+  },
+  memoHeader: {
+    backgroundColor: '#46b',
+    height: 96,
+    justifyContent: 'center',
+    paddingVertical: 24,
+    paddingHorizontal: 19
+  },
+  memoTitle: {
+    color: '#ffffff',
+    fontSize: 20,
+    lineHeight: 32,
+    fontWeight: 'bold'
+  },
+  memoDate: {
+    color: '#ffffff',
+    fontSize: 12,
+    lineHeight: 16
+  },
+  memoBody: {
+    paddingHorizontal: 27,
+    paddingVertical: 32
+  },
+  memoBodyText: {
+    fontSize: 16,
+    lineHeight: 24
+  }
+})
+
+export default Detail
--- src/app/memo/list.tsx
+++ src/app/memo/list.tsx
@@ -1,8 +1,10 @@
 import { View, StyleSheet } from 'react-native'
+// import { Feather } from '@expo/vector-icons'
 
 import Header from '../../components/header'
 import MemoListItem from '../../components/MemoListItem'
 import CircleButton from '../../components/CircleButton'
+import Icon from '../../components/Icon'
 
 const Index = (): JSX.Element => {
   return (
@@ -18,7 +20,10 @@ const Index = (): JSX.Element => {
 
       </View>
 
-      <CircleButton>+</CircleButton>
+      <CircleButton>
+        {/* <Feather name="plus" size={40} color="#ffffff" /> */}
+        <Icon name='plus' size={40} color='#ffffff'/>
+      </CircleButton>
 
     </View>
   )
